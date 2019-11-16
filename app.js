8ab04b8136d2b162f603d0e8361c6d35c40a4ba5
Ostap_Hembara <gembara1999@gmail.com>
2019-11-16 22:43:23 +0200

    *BackEnd*: added <Routers>, <Controllers>, <Models>

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const analyticsRoutes = require('./routes/analytics')
+const authRoutes = require('./routes/auth')
+const categoryRoutes = require('./routes/category')
+const orderRoutes = require('./routes/order')
+const positionRoutes = require('./routes/position')
+const keys = require('./config/keys')
+const app = express()
+
+mongoose.connect(keys.mongoDbURI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(error => console.error(error))
+
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(require('cors')())
+
+app.use('/api/analytics', analyticsRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/position', positionRoutes)
+
+module.exports = app
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoDbURI: 'mongodb+srv://ostapko:A123456@crmsystem-p8kio.mongodb.net/test?retryWrites=true&w=majority'
+}
